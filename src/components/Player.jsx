53bfd6805f7d823dc6fd472d63ed86605545e5db
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-01-06 11:35:29 +0800

    handle playerInputChange and editButtonClick logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,28 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = function () {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = function (event) {
+    setPlayerName(event.target.value);
+  };
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input type="text" value={playerName} onChange={handleChange} />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
