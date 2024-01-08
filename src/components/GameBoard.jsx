e8ec014e07dddccde20aed518715bb3b48f06aee
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-01-08 16:39:12 +0800

    add winning-combinations data and logic of checking winner

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,35 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   const handleSelectSquare = function (rowIndex, colIndex) {
-  //     setGameBoard((prevGameBoard) => {
-  //       const updatedGameboard = [
-  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //       ];
-  //       updatedGameboard[rowIndex][colIndex] = activePlayerSymbol;
-  //       return updatedGameboard;
-  //     });
-
-  //     onSelectSquare();
-  //   };
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
