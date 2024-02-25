6c19a7cd9c16b5ccdd74c0b8a490aa5b7499de17
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-02-25 20:40:07 +0800

    fix: update Vite base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/react-tic-tac-toe/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/react-tic-tac-toe/";
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  base: "/react-tic-tac-toe",
 });
